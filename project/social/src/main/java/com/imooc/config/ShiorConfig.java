9e2ed321320ab4cc524cb75a9a078bbfed04f7fd
fulinhu <1205668006@qq.com>
2018-05-30 17:55:51 +0800

    ajax  权限与shiro的结合

--- project/social/src/main/java/com/imooc/config/ShiorConfig.java
+++ project/social/src/main/java/com/imooc/config/ShiorConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.apache.shiro.mgt.SecurityManager;
 
 
+import javax.servlet.Filter;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -49,6 +50,10 @@ public class ShiorConfig {
         // 未授权界面;
         shiroFilterFactoryBean.setUnauthorizedUrl("/403");
 
+        Map<String, Filter> filters = shiroFilterFactoryBean.getFilters();
+        //自定义，当没权限时   区分ajax和普通请求的界面
+        filters.put("perms",new ShiroPermissionsFilter());
+
         // 拦截器.
         Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
         // 配置不会被拦截的链接 顺序判断
@@ -62,7 +67,9 @@ public class ShiorConfig {
         // 配置退出过滤器,其中的具体的退出代码Shiro已经替我们实现了
         filterChainDefinitionMap.put("/logout", "logout");
 
-        filterChainDefinitionMap.put("/add", "perms[权限添加]");
+        filterChainDefinitionMap.put("/users/add", "perms[权限添加]");
+
+
 
         // <!-- 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
         // <!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
